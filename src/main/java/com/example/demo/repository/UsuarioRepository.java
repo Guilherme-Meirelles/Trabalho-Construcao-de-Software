2791781360ec5d56ea15fc78e8aed079a7a5d03b
Guilherme-Meirelles <guijosemeirelles@gmail.com>
2025-11-02 18:33:29 -0300

    1

---
+++ src/main/java/com/example/demo/repository/UsuarioRepository.java
@@ -0,0 +1,12 @@
+package com.example.demo.repository;
+import com.example.demo.Modelos.Usuario;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.web.bind.annotation.*;
+public interface UsuarioRepository extends CrudRepository<Usuario, String> {
+
+    Usuario findById(Long id);
+
+    @Query(value = "select * from todaily_db.usuario where email = :email and senha = :senha", nativeQuery = true)
+    public Usuario login(String email, String senha);
+}
