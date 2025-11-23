7320c2d1e56b0a9373936aca654045191121c3fa
Guilherme-Meirelles <guijosemeirelles@gmail.com>
2025-11-23 16:17:10 -0300

    Pull do commit do enzo

--- src/main/java/com/example/demo/ToDailyApplication.java
+++ src/main/java/com/example/demo/ToDailyApplication.java
@@ -3,11 +3,13 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
 public class ToDailyApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ToDailyApplication.class, args);
+		SpringApplication.run(ToDailyApplication.class, Arrays.toString(args));
 	}
 
 }
