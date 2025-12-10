df8e427cd4ec895c0c8068ccf3b1c9a0f35cdfd1
Guilherme-Meirelles <guijosemeirelles@gmail.com>
2025-12-09 23:47:53 -0300

    Erro do botao de adicionar area de trabalho consertado

--- src/main/java/com/example/demo/Controles/AreaTrabalhoController.java
+++ src/main/java/com/example/demo/Controles/AreaTrabalhoController.java
@@ -67,10 +67,13 @@ public class AreaTrabalhoController {
         model.addAttribute("dataNascimento", usuario.getDataNascimento());
 
         List<ParticipacaoArea> participacaoAreas = participacaoAreaRepository.findByUsuario(usuario);
-
+        List<AreaTrabalho> areas = new ArrayList<AreaTrabalho>();
+        for (ParticipacaoArea participacaoArea : participacaoAreas) {
+            areas.add(participacaoArea.getArea());
+        }
         model.addAttribute("usuario", usuario);
         model.addAttribute("isMenu", true);
-        model.addAttribute("areas", participacaoAreas);
+        model.addAttribute("areas", areas);
 
         return "areasTrabalho";
     }
